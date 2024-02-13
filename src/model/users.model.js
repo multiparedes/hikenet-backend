d8ac0500fce4b98defba06a6f0fe58b970668b87
Martí Paredes <martiparedes19@gmail.com>
2024-02-13 22:41:19 +0100

    Users CRUD

---
+++ src/model/users.model.js
@@ -0,0 +1,50 @@
+const { Sequelize, DataTypes, Model } = require("sequelize");
+
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: 3306,
+  }
+);
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+  }
+);
+
+module.exports = User;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,46 +1,103 @@
 const { Router } = require("express");
 const router = Router();
-const queryUtils = require("../services/queryUtils");
 
-router.get("/", async (req, res) => {
-  const currentPage = req.query.page ?? 1;
+// DEVELOP, CREATE FAKE DATA
+const { faker } = require("@faker-js/faker");
 
-  if (currentPage === "0") {
-    res.status(400).json({ message: "The page should be greather than zero" });
-    return;
-  }
+// Impor the sequelize table
+const Users = require("../model/users.model");
+// Check if the table exists, create if not.
+Users.sync({});
 
+function errorFunction(res) {
+  return res.status(500).json({ message: "HELP! Something broke 🧯🔥" });
+}
+
+router
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      const users = await Users.findAll();
+
+      res.json(users);
+    } catch (error) {
+      console.log(error);
+      return errorFunction(res);
+    }
+  })
+  .post(postUser);
+
+async function getUser(req, res) {
   try {
-    const query = await queryUtils.getMultiple(
-      "SELECT * FROM users",
-      currentPage
-    );
-    res.json({
-      message: `Information list`,
-      ...query,
-    });
+    const user = await Users.findOne({ where: { id: req.params.id } });
+
+    res.json(user ?? { message: "User not found 😞" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
-
-    return;
+    return errorFunction(res);
   }
-});
+}
 
-router.route("/:id").get(async (req, res) => {
+async function postUser(req, res) {
   try {
-    const query = await queryUtils.getSingle(
-      `SELECT * FROM users WHERE ID = ${req.params.id}`
+    const newUser = await Users.create({
+      username: faker.internet.userName(),
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      isAdmin: faker.datatype.boolean(),
+    });
+
+    res.json(newUser);
+  } catch (error) {
+    console.log(error);
+    return errorFunction(res);
+  }
+}
+
+async function deleteUser(req, res) {
+  try {
+    const deletedUser = await Users.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.json(
+      deletedUser == 0
+        ? { message: "User not found 😞" }
+        : { message: "User deleted successfully 😃" }
+    );
+  } catch (error) {
+    console.log(error);
+    return errorFunction(res);
+  }
+}
+
+async function patchUser(req, res) {
+  try {
+    const updatedUser = await Users.update(
+      {
+        ...req.body,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
     );
 
-    res.json({
-      message: `Information for user ${req.params.id}`,
-      ...query,
-    });
+    res.json(
+      updatedUser == 0
+        ? { message: "User not found 😞" }
+        : { message: "User updated successfully 😃" }
+    );
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
     console.log(error);
+    return errorFunction(res);
   }
-});
+}
+
+router.route("/:id").get(getUser).patch(patchUser).delete(deleteUser);
 
 module.exports = router;
