a1ebfe556e6cecc14cdb3beaa72164667dd39862
Marti Paredes <martiparedes19@gmail.com>
2024-02-16 20:38:52 +0100

    Docker setup for dev and prod

--- src/index.js
+++ src/index.js
@@ -35,5 +35,5 @@ app.use("/users", authMiddleware, userRoutes);
 
 // Iniciando el servidor
 app.listen(app.get("port"), () => {
-  console.log(`Server listening on port ${app.get("port")}`);
+  console.log(`Server listening on port ${app.get("port")} inside docker!`);
 });
