fc52fc58db3473033787c71fd594fa5653eb7b3b
Marti Paredes <martiparedes19@gmail.com>
2024-02-18 19:22:30 +0100

    War is over - Docker

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
   process.env.MYSQLDB_USER,
   process.env.MYSQLDB_PASSWORD,
   {
+    host: process.env.MYSQLDB_HOST,
     dialect: "mysql",
     port: process.env.MYSQLDB_DOCKER_PORT,
   },
